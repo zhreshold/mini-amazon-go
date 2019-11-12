4f63e2150ee98c28fc56d607ae1663ec0ef5f72d
EC2 Default User <ec2-user@ip-172-16-50-135.us-west-2.compute.internal>
2019-11-12 07:00:31 +0000

    change ip

--- src/start_upload_rtmp.sh
+++ src/start_upload_rtmp.sh
@@ -1,2 +1,2 @@
-ffmpeg -f v4l2 -r 45 -video_size vga -pix_fmt yuv420p12be -i /dev/video0 -b:v 500k -c:v h264_omx -preset ultrafast -an -f flv rtmp://ec2-52-53-216-142.us-west-1.compute.amazonaws.com/live/123456
+ffmpeg -f v4l2 -r 45 -video_size vga -pix_fmt yuv420p12be -i /dev/video0 -b:v 500k -c:v h264_omx -preset ultrafast -an -f flv rtmp://ec2-54-222-130-39.cn-north-1.compute.amazonaws.com.cn/live/123456
 
